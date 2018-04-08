958144366c309cf7a9c20ba0e20c3d5e0a138bb9
Sonickers <35639449+Sonickers@users.noreply.github.com>
2018-04-08 11:45:19 +0200

    x

--- scripts.js
+++ scripts.js
@@ -3,13 +3,16 @@ $(document).ready(function() {
 		const models = json.models;
 		const engines = json.engines;
 		const gearboxes = json.gearboxes;
+		const colors = json.colors;
 
 		const modelDiv = $('#models');
 		models.forEach(function(model) {
 			const btnTemplate = $('.js-btn-template > button').clone();
 			btnTemplate.html(model.name);
 			btnTemplate.attr('disabled', false);
+
 			btnTemplate.on('click', function() {
+				$('.right-model').html(model.name);
 				$('#models button, #engines button, #gearboxes button').removeClass('active');
 				$(this).addClass('active');
 				$('#engines button, #gearboxes button').attr('disabled', true);
@@ -25,6 +28,7 @@ $(document).ready(function() {
 			const btnTemplate = $('.js-btn-template > button').clone();
 			btnTemplate.html(engine.name);
 			btnTemplate.on('click', function() {
+				$('.right-engine').html(engine.name);
 				$('#engines button, #gearboxes button').removeClass('active');
 				$(this).addClass('active');
 				$('#gearboxes button').attr('disabled', true);
@@ -35,24 +39,39 @@ $(document).ready(function() {
 			engineDiv.append(btnTemplate);
 		});
 
+		const colorsDiv = $('#colors');
+		colors.forEach(function(color) {
+			const name = color.name;
+			const rgb = color.rgb;
+			const btnTemplate = $('.js-btn-template > button').clone();
+			btnTemplate.css('background-color', rgb);
+			btnTemplate.html('&nbsp;');
+			btnTemplate.attr('disabled', false);
+			btnTemplate.removeClass('btn-car-selector');
+			btnTemplate.addClass('btn-color');
+
+			btnTemplate.on('click', function()	{
+				$(".btn-color").removeClass("active");
+				$(this).addClass("active");
+				$(".fa-car").css('color', rgb);
+				$('.right-color').html(name);
+			});
+			colorsDiv.append(btnTemplate);
+		});
+
+		$('.btn-color').eq(0).click();
+
 		const gearboxDiv = $('#gearboxes');
 		gearboxes.forEach(function(gearbox) {
 			const btnTemplate = $('.js-btn-template > button').clone();
 			btnTemplate.html(gearbox.name);
 			btnTemplate.on('click', function() {
+				$('.right-gearbox').html(gearbox.name);
 				$('#gearboxes button').removeClass('active');
 				$(this).addClass('active');
 			});
 			gearboxDiv.append(btnTemplate);
 		});
-	});
-
-	$('.btn-color').on('click', function()	{
-		const color = $(this).data('color');
-		const name = $(this).data('name');
-		$(".btn-color").removeClass("active");
-		$(this).addClass("active");
-		$(".fa-car").css('color', color);
 	});
 
 	
