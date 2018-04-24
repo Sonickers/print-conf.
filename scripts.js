faf6d6da2e7e69513710b2841fe708ecd62618d8
Sonickers <35639449+Sonickers@users.noreply.github.com>
2018-04-24 13:19:21 +0200

    change evth

--- scripts.js
+++ scripts.js
@@ -1,56 +1,56 @@
 $(document).ready(function() {
 	function totalPrice() {
 		var sum = 0;
-		$('.btn-car-selector.active').each(function() {
+		$('.btn-stethoscope-selector.active').each(function() {
 			sum += parseInt($(this).data('price'));
 			$('.right-price').html(sum);
 		});
 	}
 
 	$.getJSON('data.json', function(json) {
-		const models = json.models;
-		const engines = json.engines;
-		const gearboxes = json.gearboxes;
+		const brands = json.brands;
+		const models = json.models;		
+		const personalisations = json.personalisations;
 		const colors = json.colors;
+
+		const brandDiv = $('#brands');
+		brands.forEach(function(brand) {
+			const btnTemplate = $('.js-btn-template > button').clone();
+			btnTemplate.html(brand.name);
+			btnTemplate.attr('disabled', false);
+			btnTemplate.data('price', brand.price);
+
+			btnTemplate.on('click', function() {
+				$('.right-brand').html(brand.name);
+				$('#brands button, #models button, #personalisations button').removeClass('active');
+				$(this).addClass('active');
+				$('.right-model, .right-personalisation').html('');
+				$('#models button, #personalisations button').attr('disabled', true);
+				brand.models.forEach(function(index) {
+					$('#models button').eq(index).attr('disabled', false);
+				});
+				totalPrice();
+			});
+			brandDiv.append(btnTemplate);
+		});
 
 		const modelDiv = $('#models');
 		models.forEach(function(model) {
 			const btnTemplate = $('.js-btn-template > button').clone();
 			btnTemplate.html(model.name);
-			btnTemplate.attr('disabled', false);
 			btnTemplate.data('price', model.price);
-
 			btnTemplate.on('click', function() {
 				$('.right-model').html(model.name);
-				$('#models button, #engines button, #gearboxes button').removeClass('active');
+				$('#models button, #presonalisations button').removeClass('active');
+				$('.right-personalisation').html('');
 				$(this).addClass('active');
-				$('.right-engine, .right-gearbox').html('');
-				$('#engines button, #gearboxes button').attr('disabled', true);
-				model.engines.forEach(function(index) {
-					$('#engines button').eq(index).attr('disabled', false);
+				$('#personalisations button').attr('disabled', true);
+				model.personalisations.forEach(function(index) {
+					$('#personalisations button').eq(index).attr('disabled', false);
 				});
 				totalPrice();
 			});
 			modelDiv.append(btnTemplate);
-		});
-
-		const engineDiv = $('#engines');
-		engines.forEach(function(engine) {
-			const btnTemplate = $('.js-btn-template > button').clone();
-			btnTemplate.html(engine.name);
-			btnTemplate.data('price', engine.price);
-			btnTemplate.on('click', function() {
-				$('.right-engine').html(engine.name);
-				$('#engines button, #gearboxes button').removeClass('active');
-				$('.right-gearbox').html('');
-				$(this).addClass('active');
-				$('#gearboxes button').attr('disabled', true);
-				engine.gearboxes.forEach(function(index) {
-					$('#gearboxes button').eq(index).attr('disabled', false);
-				});
-				totalPrice();
-			});
-			engineDiv.append(btnTemplate);
 		});
 
 		const colorsDiv = $('#colors');
@@ -61,13 +61,13 @@ $(document).ready(function() {
 			btnTemplate.css('background-color', rgb);
 			btnTemplate.html('&nbsp;');
 			btnTemplate.attr('disabled', false);
-			btnTemplate.removeClass('btn-car-selector');
+			btnTemplate.removeClass('btn-stethoscope-selector');
 			btnTemplate.addClass('btn-color');
 
 			btnTemplate.on('click', function()	{
 				$(".btn-color").removeClass("active");
 				$(this).addClass("active");
-				$(".fa-car").css('color', rgb);
+				$(".fa-stethoscope").css('color', rgb);
 				$('.right-color').html(name);
 			});
 			colorsDiv.append(btnTemplate);
@@ -75,18 +75,18 @@ $(document).ready(function() {
 
 		$('.btn-color').eq(0).click();
 
-		const gearboxDiv = $('#gearboxes');
-		gearboxes.forEach(function(gearbox) {
+		const personalisationDiv = $('#personalisations');
+		personalisations.forEach(function(personalisation) {
 			const btnTemplate = $('.js-btn-template > button').clone();
-			btnTemplate.html(gearbox.name);
-			btnTemplate.data('price', gearbox.price);
+			btnTemplate.html(personalisation.name);
+			btnTemplate.data('price', personalisation.price);
 			btnTemplate.on('click', function() {
-				$('.right-gearbox').html(gearbox.name);
-				$('#gearboxes button').removeClass('active');
+				$('.right-personalisation').html(personalisation.name);
+				$('#personalisations button').removeClass('active');
 				$(this).addClass('active');
 				totalPrice();
 			});
-			gearboxDiv.append(btnTemplate);
+			personalisationDiv.append(btnTemplate);
 		});
 	});
 });
